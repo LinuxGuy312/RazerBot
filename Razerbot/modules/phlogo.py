ef76dde3a88b96388411501197cc460234fba3ed
LinuxGuy312 <101087324+LinuxGuy312@users.noreply.github.com>
2022-11-21 16:06:53 +0530

    jaadu

--- Razerbot/modules/phlogo.py
+++ Razerbot/modules/phlogo.py
@@ -9,12 +9,22 @@ except ModuleNotFoundError:
 
 @register(pattern="^/phlogo ?(.*)")
 async def ph(event):
-    query = event.pattern_match.group(1)
-    p = query.split(" ", 1)[0]
-    h = query.split(" ", 1)[1]
-    reply_to_id = event.reply_to_msg_id
-    result = generate(f"{p}",f"{h}")
-    result.save("ph.png")
-    pic = "/root/Razerbot/ph.png"
-    await event.client.send_file(event.chat_id, pic, reply_to=event.reply_to_msg_id, forcedocument=False)
-    os.remove(pic)
+	query = event.pattern_match.group(1)
+	if query == "":
+		o = event.edit("Give some text bruh, e.g.: `/phlogo Razer Bot`")
+		o.delete()
+		return
+	try:
+		p = query.split(" ", 1)[0]
+		h = query.split(" ", 1)[1]
+	except:
+		o = event.edit("Something went wrong, try giving two words. e.g.: `/phlogo Razer Bot`")
+		o.delete()
+		return
+	await edit_delete(event, "Processing...")
+	reply_to_id = event.reply_to_msg_id
+	result = generate(f"{p}",f"{h}")
+	pic = "ph.png"
+	result.save(pic, "png")
+	await event.client.send_file(event.chat_id, pic, reply_to=event.reply_to_msg_id, forcedocument=False)
+	os.remove(pic)
