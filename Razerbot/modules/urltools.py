6ef615944d4fb0678df8c476a4508651f22f8832
LinuxGuy312 <githubwh@gmail.com>
2022-11-22 12:43:18 +0530

    no button is good

--- Razerbot/modules/urltools.py
+++ Razerbot/modules/urltools.py
@@ -2,7 +2,6 @@ import requests
 from validators.url import url
 from Razerbot import telethn as tbot
 from Razerbot.events import register
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 
 @register(pattern="^/dns(?:\s|$)([\s\S]*)")
 async def _(event):
@@ -45,8 +44,7 @@ async def _(event):
         input_str = f"http://{input_str}"
     sample_url = f"https://da.gd/s?url={input_str}"
     if response_api := requests.get(sample_url).text:
-        button = [InlineKeyboardButton(text="ᴠɪsɪᴛ", url=f"{response_api}")]
-        await event.reply(f"sʜᴏʀᴛᴇɴᴇᴅ ᴜʀʟ:\n\n`{response_api}`".format(reply_markup=InlineKeyboardMarkup(button)))
+        await event.reply(f"sʜᴏʀᴛᴇɴᴇᴅ ᴜʀʟ:\n\n`{response_api}`")
     else:
         await event.reply("sᴏᴍᴇᴛʜɪɴɢ ɪs ᴡʀᴏɴɢ, ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ.")
 
@@ -70,8 +68,7 @@ async def _(event):
         input_str = f"http://{input_str}"
     r = requests.get(input_str, allow_redirects=False)
     if str(r.status_code).startswith("3"):
-        button = [InlineKeyboardButton(text="ᴠɪsɪᴛ", url=f"{r.headers['Location']}")]
-        await event.reply(f"ʀᴇᴅɪʀᴇᴄᴛᴇᴅ ᴜʀʟ:\n\n`{r.headers['Location']}`".format(reply_markup=InlineKeyboardMarkup(button)))
+        await event.reply(f"ʀᴇᴅɪʀᴇᴄᴛᴇᴅ ᴜʀʟ:\n\n`{r.headers['Location']}`")
     else:
         await event.reply(f"ɪɴᴘᴜᴛ ᴜʀʟ {input_str} ʀᴇᴛᴜʀɴᴇᴅ sᴛᴀᴛᴜs_ᴄᴏᴅᴇ {r.status_code}")
 
