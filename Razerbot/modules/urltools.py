21097f00a9156c4534537f10c7faaeca2c7484b8
LinuxGuy312 <githubwh@gmail.com>
2022-11-22 11:44:08 +0530

    pls worl

--- Razerbot/modules/urltools.py
+++ Razerbot/modules/urltools.py
@@ -46,7 +46,8 @@ async def _(event):
         input_str = f"http://{input_str}"
     sample_url = f"https://da.gd/s?url={input_str}"
     if response_api := requests.get(sample_url).text:
-        await event.reply(f"sʜᴏʀᴛᴇɴᴇᴅ ᴜʀʟ:\n\n`{response_api}`", buttons=[Button.url('ᴠɪsɪᴛ', f'{response_api}')])
+        url = f"{response_api}"
+        await event.reply(f"sʜᴏʀᴛᴇɴᴇᴅ ᴜʀʟ:\n\n`{response_api}`", buttons=[Button.url('ᴠɪsɪᴛ', url)])
     else:
         await event.reply("sᴏᴍᴇᴛʜɪɴɢ ɪs ᴡʀᴏɴɢ, ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ.")
 
@@ -70,7 +71,8 @@ async def _(event):
         input_str = f"http://{input_str}"
     r = requests.get(input_str, allow_redirects=False)
     if str(r.status_code).startswith("3"):
-        await event.reply(f"ʀᴇᴅɪʀᴇᴄᴛᴇᴅ ᴜʀʟ:\n\n`{r.headers['Location']}`", buttons=[Button.url('ᴠɪsɪᴛ', f"{r.headers['Location']}")])
+        redurl = f"{r.headers['Location']}"
+        await event.reply(f"ʀᴇᴅɪʀᴇᴄᴛᴇᴅ ᴜʀʟ:\n\n`{r.headers['Location']}`", buttons=[Button.url('ᴠɪsɪᴛ', redurl)])
     else:
         await event.reply(f"ɪɴᴘᴜᴛ ᴜʀʟ {input_str} ʀᴇᴛᴜʀɴᴇᴅ sᴛᴀᴛᴜs_ᴄᴏᴅᴇ {r.status_code}")
 
