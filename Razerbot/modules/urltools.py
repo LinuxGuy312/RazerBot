a039e1787ae6a676ef5669f210b035a9f5f72cc2
LinuxGuy312 <githubwh@gmail.com>
2022-11-22 12:02:35 +0530

    no buttons is goood

--- Razerbot/modules/urltools.py
+++ Razerbot/modules/urltools.py
@@ -2,7 +2,6 @@ import requests
 from validators.url import url
 from Razerbot import telethn as tbot
 from Razerbot.events import register
-from telethon.tl.custom import Button
 
 
 @register(pattern="^/dns(?:\s|$)([\s\S]*)")
@@ -47,7 +46,7 @@ async def _(event):
     sample_url = f"https://da.gd/s?url={input_str}"
     if response_api := requests.get(sample_url).text:
         resurl = f"{response_api}"
-        await event.reply(f"sʜᴏʀᴛᴇɴᴇᴅ ᴜʀʟ:\n\n`{response_api}`", buttons=[Button.inline('ᴠɪsɪᴛ', url=resurl)])
+        await event.reply(f"sʜᴏʀᴛᴇɴᴇᴅ ᴜʀʟ:\n\n`{response_api}`")
     else:
         await event.reply("sᴏᴍᴇᴛʜɪɴɢ ɪs ᴡʀᴏɴɢ, ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ.")
 
@@ -72,7 +71,7 @@ async def _(event):
     r = requests.get(input_str, allow_redirects=False)
     if str(r.status_code).startswith("3"):
         redurl = f"{r.headers['Location']}"
-        await event.reply(f"ʀᴇᴅɪʀᴇᴄᴛᴇᴅ ᴜʀʟ:\n\n`{r.headers['Location']}`", buttons=[Button.inline('ᴠɪsɪᴛ', url=redurl)])
+        await event.reply(f"ʀᴇᴅɪʀᴇᴄᴛᴇᴅ ᴜʀʟ:\n\n`{r.headers['Location']}`")
     else:
         await event.reply(f"ɪɴᴘᴜᴛ ᴜʀʟ {input_str} ʀᴇᴛᴜʀɴᴇᴅ sᴛᴀᴛᴜs_ᴄᴏᴅᴇ {r.status_code}")
 
