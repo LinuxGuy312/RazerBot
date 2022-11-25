e8082554be87f6e0464b4622b61b2f02ed36d764
LinuxGuy312 <githubwh@gmail.com>
2022-11-25 22:29:14 +0530

    webss

--- Razerbot/modules/webss.py
+++ Razerbot/modules/webss.py
@@ -1,30 +1,67 @@
-from pyrogram import filters
-from pyrogram.types import Message
+import io
+import traceback
+from datetime import datetime
 
-from Razerbot import pbot as app
-from Razerbot.utils.errors import capture_err
+import requests
+from selenium import webdriver
+from validators.url import url
 
-__mod_name__ = "WebSS"
-__help__ = "➢ /webss | .webss [URL] - Take A Screenshot Of A Webpage"
+from Razerbot import telethn as tbot
+from Razerbot import register
 
 
-@app.on_message(filters.command("webss"))
-@capture_err
-async def take_ss(_, message: Message):
+@register(pattern="/(webss|gis) ([\s\S]*)")
+async def _(event):
+    "To Take a screenshot of a website."
+    CHROME_BIN = "/usr/bin/google-chrome"
+    razevent = await event.reply('`Processing..`')
+    start = datetime.now()
     try:
-        if len(message.command) != 2:
-            return await message.reply_text("ɢɪᴠᴇ ᴀ ᴜʀʟ ᴛᴏ ꜰᴇᴛᴄʜ sᴄʀᴇᴇɴsʜᴏᴛ.")
-        url = message.text.split(None, 1)[1]
-        m = await message.reply_text("ᴛᴀᴋɪɴɢ sᴄʀᴇᴇɴsʜᴏᴛ..")
-        await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
-        try:
-            await message.send_photo(
-                photo=f"https://webshot.amanoteam.com/print?q={url}",
-                quote=True,
-                reply_to=message.id
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--ignore-certificate-errors")
+        chrome_options.add_argument("--test-type")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.binary_location = CHROME_BIN
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+        cmd = event.pattern_match.group(1)
+        input_str = event.pattern_match.group(2)
+        inputstr = input_str
+        if cmd == "ss":
+            razurl = url(inputstr)
+            if not razurl:
+                inputstr = f"http://{input_str}"
+                razurl = url(inputstr)
+            if not razurl:
+                return await razevent.edit("`The given input is not supported url`")
+        if cmd == "gis":
+            inputstr = f"https://www.google.com/search?q={input_str}"
+        driver.get(inputstr)
+        await catevent.edit("`Calculating Page Dimensions`")
+        height = driver.execute_script(
+            "return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);"
+        )
+        width = driver.execute_script(
+            "return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);"
+        )
+        driver.set_window_size(width + 100, height + 100)
+        im_png = driver.get_screenshot_as_png()
+        driver.close()
+        end = datetime.now()
+        ms = (end - start).seconds
+        hmm = f"**URL : **{input_str} \n**Time :** `{ms} seconds`"
+        await catevent.delete()
+        with io.BytesIO(im_png) as out_file:
+            out_file.name = f"{input_str}.PNG"
+            await tbot.send_file(
+                event.chat_id,
+                out_file,
+                caption=hmm,
+                force_document=True,
+                reply_to=event.message.id,
+                allow_cache=False,
+                silent=True,
             )
-        except TypeError:
-            return await m.edit("ɴᴏ sᴜᴄʜ ᴡᴇʙsɪᴛᴇ.")
-        await m.delete()
-    except Exception as e:
-        await message.reply_text(str(e))
+    except Exception:
+        await razevent.edit(f"`{traceback.format_exc()}`")
