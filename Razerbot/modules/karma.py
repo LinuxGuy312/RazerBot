7c05b2089067f07ba6797af762fc43b995e8015e
LinuxGuy312 <githubwh@gmail.com>
2023-01-01 09:10:46 +0530

    few stuff

---
+++ Razerbot/modules/karma.py
@@ -0,0 +1,159 @@
+import asyncio
+
+from pyrogram import filters
+
+from Razerbot import OWNER_ID, pbot
+from Razerbot.utils.errors import capture_err
+from Razerbot.utils.dbfunctions import (
+    alpha_to_int,
+    get_karma,
+    get_karmas,
+    int_to_alpha,
+    is_karma_on,
+    karma_off,
+    karma_on,
+    update_karma,
+)
+
+regex_upvote = r"^((?i)\+|\+\+|\+1|\+69|thx|thanx|thanks|🖤|❣️|💝|💖|💕|❤|💘|cool|good|👍|baby|thankyou|love|pro)$"
+regex_downvote = r"^(\-|\-\-|\-1|👎|💔|noob|weak|fuck off|nub|gey|kid|shit|mf)$"
+
+karma_positive_group = 3
+karma_negative_group = 4
+
+
+@pbot.on_message(
+    filters.text
+    & filters.group
+    & filters.incoming
+    & filters.reply
+    & filters.regex(regex_upvote)
+    & ~filters.via_bot
+    & ~filters.bot,
+    group=karma_positive_group,
+)
+@capture_err
+async def upvote(_, message):
+    if not await is_karma_on(message.chat.id):
+        return
+    if not message.reply_to_message.from_user:
+        return
+    if not message.from_user:
+        return
+    if message.reply_to_message.from_user.id == OWNER_ID:
+        await message.reply_text("ᴍʏ ᴏᴡɴᴇʀ ʜᴀs ꝏ ᴋᴀʀᴍᴀ ᴘᴏɪɴᴛs.")
+        return
+    if message.reply_to_message.from_user.id == message.from_user.id:
+        return
+    chat_id = message.chat.id
+    user_id = message.reply_to_message.from_user.id
+    user_mention = message.reply_to_message.from_user.mention
+    current_karma = await get_karma(chat_id, await int_to_alpha(user_id))
+    if current_karma:
+        current_karma = current_karma["karma"]
+        karma = current_karma + 1
+    else:
+        karma = 1
+    new_karma = {"karma": karma}
+    await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
+    await message.reply_text(
+        f"ɪɴᴄʀᴇᴍᴇɴᴛᴇᴅ ᴋᴀʀᴍᴀ ᴏғ {user_mention} ʙʏ 1.\n**ᴛᴏᴛᴀʟ ᴩᴏɪɴᴛs :** {karma}"
+    )
+
+
+@pbot.on_message(
+    filters.text
+    & filters.group
+    & filters.incoming
+    & filters.reply
+    & filters.regex(regex_downvote)
+    & ~filters.via_bot
+    & ~filters.bot,
+    group=karma_negative_group,
+)
+@capture_err
+async def downvote(_, message):
+    if not await is_karma_on(message.chat.id):
+        return
+    if not message.reply_to_message.from_user:
+        return
+    if not message.from_user:
+        return
+    if message.reply_to_message.from_user.id == OWNER_ID:
+        await message.reply_text("ᴍʏ ᴏᴡɴᴇʀ ʜᴀs ꝏ ᴋᴀʀᴍᴀ ᴘᴏɪɴᴛs, ᴄᴀɴ'ᴛ ᴍᴀᴋᴇ ᴀɴʏ ᴅᴇᴄʀᴇᴍᴇɴᴛ ʜᴀʜᴀʜᴀ!")
+        return
+    if message.reply_to_message.from_user.id == message.from_user.id:
+        return
+    user_id = message.reply_to_message.from_user.id
+    user_mention = message.reply_to_message.from_user.mention
+    current_karma = await get_karma(message.chat.id, await int_to_alpha(user_id))
+    if current_karma:
+        current_karma = current_karma["karma"]
+        karma = current_karma - 1
+    else:
+        karma = 0
+    new_karma = {"karma": karma}
+    await update_karma(message.chat.id, await int_to_alpha(user_id), new_karma)
+    await message.reply_text(
+        f"ᴅᴇᴄʀᴇᴍᴇɴᴛᴇᴅ ᴋᴀʀᴍᴀ ᴏғ {user_mention} ʙʏ 1.\n**ᴛᴏᴛᴀʟ ᴩᴏɪɴᴛs :** {karma}"
+    )
+
+
+@pbot.on_message(filters.command("karmastat") & filters.group)
+@capture_err
+async def karma(_, message):
+    if not message.reply_to_message:
+        m = await message.reply_text("Analyzing Karma...Will Take 10 Seconds")
+        karma = await get_karmas(message.chat.id)
+        if not karma:
+            await m.edit_text("No karma in DB for this chat.")
+            return
+        msg = f"**Karma list of {message.chat.title} :**\n"
+        limit = 0
+        karma_dicc = {}
+        for i in karma:
+            user_id = await alpha_to_int(i)
+            user_karma = karma[i]["karma"]
+            karma_dicc[str(user_id)] = user_karma
+            karma_arranged = dict(
+                sorted(karma_dicc.items(), key=lambda item: item[1], reverse=True)
+            )
+        if not karma_dicc:
+            await m.edit_text("No karma in DB for this chat.")
+            return
+        for user_idd, karma_count in karma_arranged.items():
+            if limit > 9:
+                break
+            try:
+                user = await app.get_users(int(user_idd))
+                await asyncio.sleep(0.8)
+            except Exception:
+                continue
+            first_name = user.first_name
+            if not first_name:
+                continue
+            msg += f"`{karma_count}`  {(first_name[0:12] + '...') if len(first_name) > 12 else first_name}\n"
+            limit += 1
+        await m.edit_text(msg)
+    else:
+        user_id = message.reply_to_message.from_user.id
+        karma = await get_karma(message.chat.id, await int_to_alpha(user_id))
+        karma = karma["karma"] if karma else 0
+        await message.reply_text(f"**ᴛᴏᴛᴀʟ ᴩᴏɪɴᴛs :** {karma}")
+
+
+@pbot.on_message(filters.command("karma") & ~filters.private)
+async def captcha_state(_, message):
+    usage = "**Usage:**\n/karma [ON|OFF]"
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
+    if state == "on":
+        await karma_on(message.chat.id)
+        await message.reply_text("Enabled karma system.")
+    elif state == "off":
+        await karma_off(message.chat.id)
+        await message.reply_text("Disabled karma system.")
+    else:
+        await message.reply_text(usage)
---
+++ Razerbot/modules/ranuser.py
@@ -0,0 +1,44 @@
+import requests
+import os
+import json
+from datetime import date
+from Razerbot import telethn, eor
+from Razerbot.events import register
+
+@register(pattern="^[/!.]ranuser")
+async def gen(event):
+	bhurr = await eor(event, "ɢᴇɴᴇʀᴀᴛɪɴɢ ɪɴꜰᴏ...")
+	yr = date.today().year
+	cyr = int(yr)
+	inf = "https://randomuser.me/api"
+	ccinf = "https://random-data-api.com/api/v2/credit_cards"
+	rinf = requests.get(inf)
+	rccinf = requests.get(ccinf)
+	injs = rinf.json()
+	ccinjs = rccinf.json()
+	tt, fn, ln = injs['results'][0]['name']['title'], injs['results'][0]['name']['first'], injs['results'][0]['name']['last']
+	gender = injs['results'][0]['gender']
+	name = f"{tt} {fn} {ln}"
+	snu, snam = injs['results'][0]['location']['street']['number'], injs['results'][0]['location']['street']['name']
+	street = f"{snu}, {snam}"
+	city = injs['results'][0]['location']['city']
+	state = injs['results'][0]['location']['state']
+	country = injs['results'][0]['location']['country']
+	pscd = injs['results'][0]['location']['postcode']
+	email = injs['results'][0]['email'].replace("example", "email")
+	cell = injs['results'][0]['cell']
+	ccnum = ccinjs['credit_card_number']
+	ccexp = ccinjs['credit_card_expiry_date']
+	cvv = random.choice(range(100,999))
+	poli = f"https://fakeface.rest/face/json?gender={gender}&minimum_age=18&maximum_age=40"
+	rpoli = requests.get(poli)
+	poinjs = rpoli.json()
+	poto = poinjs['image_url']
+	pyr = poinjs['age']
+	dyr = cyr - int(pyr)
+	dobin = injs['results'][0]['dob']['date']
+	dfm = dobin[:10][4:]
+	dob = f"{dyr}{dfm}"
+	capt = f"**ɴᴀᴍᴇ:** {name}\n**ᴅᴏʙ:** {dob}\n**sᴛʀᴇᴇᴛ:** {street}\n**ᴄɪᴛʏ:** {city}\n**sᴛᴀᴛᴇ:** {state}\n**ᴄᴏᴜɴᴛʀʏ:** {country}\n**ᴘᴏsᴛᴀʟ ᴄᴏᴅᴇ:** {pscd}\n**ᴇᴍᴀɪʟ:** {email}\n**ᴘʜᴏɴᴇ:** {cell}\n\n**ᴄᴄ ɪɴꜰᴏ:**\n    **ᴄᴄ ɴᴜᴍʙᴇʀ:** {ccnum}\n    **ᴇxᴘɪʀʏ:** {ccexp}\n    **ᴄᴠᴠ:** {cvv}"
+	await bhurr.delete()
+	await telethn.send_file(event.chat_id, poto, caption=capt, reply_to=event.reply_to_msg_id, force_document=False)
