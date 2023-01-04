97babdc476a72cb0c1f9cae551c2895e4debb379
LinuxGuy312 <githubwh@gmail.com>
2023-01-04 10:58:11 +0530

    maybe

--- Razerbot/utils/mongo.py
+++ Razerbot/utils/mongo.py
@@ -13,23 +13,23 @@ karmadb = db.karma
 
 
 async def is_afk(user_id: int) -> bool:
-    user = await usersdb.find_one({"user_id": user_id})
+    user = await afkdb.find_one({"user_id": user_id})
     if not user:
         return False, {}
     return True, user["reason"]
 
 async def add_afk(user_id: int, mode):
-    await usersdb.update_one(
+    await afkdb.update_one(
         {"user_id": user_id}, {"$set": {"reason": mode}}, upsert=True
     )
 
 async def remove_afk(user_id: int):
-    user = await usersdb.find_one({"user_id": user_id})
+    user = await afkdb.find_one({"user_id": user_id})
     if user:
-        return await usersdb.delete_one({"user_id": user_id})
+        return await afkdb.delete_one({"user_id": user_id})
 
 async def get_afk_users() -> list:
-    users = usersdb.find({"user_id": {"$gt": 0}})
+    users = afkdb.find({"user_id": {"$gt": 0}})
     if not users:
         return []
     users_list = []
