f841cd16d234be467023df797e83a2d5083b1a40
LinuxGuy312 <githubwh@gmail.com>
2023-01-05 21:05:47 +0530

    new fixes

--- Razerbot/modules/urltools.py
+++ Razerbot/modules/urltools.py
@@ -1,6 +1,7 @@
 import requests
 from validators.url import url
 from Razerbot import telethn as tbot
+from telethon import Button
 from Razerbot.events import register
 
 @register(pattern="^[!/.]dns(?:\s|$)([\s\S]*)")
@@ -44,7 +45,8 @@ async def _(event):
         input_str = f"http://{input_str}"
     sample_url = f"https://da.gd/s?url={input_str}"
     if response_api := requests.get(sample_url).text:
-        await event.reply(f"sʜᴏʀᴛᴇɴᴇᴅ ᴜʀʟ:\n\n`{response_api}`")
+        button = [Button.url("Go to shortened link", f"{response_api}")]
+        await event.reply(f"sʜᴏʀᴛᴇɴᴇᴅ ᴜʀʟ:\n\n`{response_api}`", buttons=button)
     else:
         await event.reply("sᴏᴍᴇᴛʜɪɴɢ ɪs ᴡʀᴏɴɢ, ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ.")
 
@@ -68,7 +70,8 @@ async def _(event):
         input_str = f"http://{input_str}"
     r = requests.get(input_str, allow_redirects=False)
     if str(r.status_code).startswith("3"):
-        await event.reply(f"ʀᴇᴅɪʀᴇᴄᴛᴇᴅ ᴜʀʟ:\n\n`{r.headers['Location']}`")
+        button = [Button.url("Go to Redirected Url", f"{r.headers['Location']}")]
+        await event.reply(f"ʀᴇᴅɪʀᴇᴄᴛᴇᴅ ᴜʀʟ:\n\n`{r.headers['Location']}`", buttons=button)
     else:
         await event.reply(f"ɪɴᴘᴜᴛ ᴜʀʟ {input_str} ʀᴇᴛᴜʀɴᴇᴅ sᴛᴀᴛᴜs_ᴄᴏᴅᴇ {r.status_code}")
 
@@ -89,4 +92,4 @@ async def _(event):
     if not check:
         return await event.reply("ᴛʜᴇ ɢɪᴠᴇɴ ʟɪɴᴋ ɪs ɴᴏᴛ sᴜᴘᴘᴏʀᴛᴇᴅ")
     await event.delete()
-    await event.reply(f"[ㅤㅤㅤㅤㅤㅤㅤ]({input_str})", link_preview=False)
+    await event.respond(f"[ㅤㅤㅤㅤㅤㅤㅤ]({input_str})", link_preview=False)
