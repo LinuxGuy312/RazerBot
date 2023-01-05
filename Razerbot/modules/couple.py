8a11cbbf1a6870f508b1bfcfc0cd75bae4a8aceb
LinuxGuy312 <githubwh@gmail.com>
2023-01-05 20:45:53 +0530

    previously no, but now maybe

--- Razerbot/modules/couple.py
+++ Razerbot/modules/couple.py
@@ -62,25 +62,22 @@ New couple of the day may be chosen in {rem_hrs} {rem_min} {rem_sec}"""
 New couple of the day may be chosen in {rem_hrs} {rem_min} {rem_sec}"""
             await app.send_message(message.chat.id, text=couple_selection_message)
     except Exception as e:
-        chat_id = message.chat.id
-        is_selected = await get_couple(chat_id, today)
-        if not is_selected:
-            list_of_users = []
-            async for i in app.get_chat_members(message.chat.id, limit=50):
-                if not i.user.is_bot:
-                    list_of_users.append(i.user.id)
-            if len(list_of_users) < 2:
-                return await message.reply_text("Not enough users")
+        list_of_users = []
+        async for i in app.get_chat_members(message.chat.id, limit=50):
+            if not i.user.is_bot:
+                list_of_users.append(i.user.id)
+        if len(list_of_users) < 2:
+            return await message.reply_text("Not enough users")
+        c1_id = random.choice(list_of_users)
+        c2_id = random.choice(list_of_users)
+        while c1_id == c2_id:
             c1_id = random.choice(list_of_users)
-            c2_id = random.choice(list_of_users)
-            while c1_id == c2_id:
-                c1_id = random.choice(list_of_users)
-            c1_mention = (await app.get_users(int(c1_id))).mention
-            c2_mention = (await app.get_users(int(c2_id))).mention
-            couple_selection_message = f"""Couple of the day:
+        c1_mention = (await app.get_users(int(c1_id))).mention
+        c2_mention = (await app.get_users(int(c2_id))).mention
+        couple_selection_message = f"""Couple of the day:
 {c1_mention} + {c2_mention} = ❤️
 
 New couple of the day may be chosen in {rem_hrs} {rem_min} {rem_sec}"""
-            await app.send_message(message.chat.id, text=couple_selection_message)
-            couple = {"c1_id": c1_id, "c2_id": c2_id}
-            await save_couple(chat_id, today, couple)
+        await app.send_message(message.chat.id, text=couple_selection_message)
+        couple = {"c1_id": c1_id, "c2_id": c2_id}
+        await save_couple(chat_id, today, couple)
