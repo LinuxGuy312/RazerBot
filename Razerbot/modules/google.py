609a0c3b13de5dc8744d40e831312408f88e65ff
LinuxGuy312 <githubwh@gmail.com>
2023-01-05 11:02:21 +0530

    grs cmd added maybe

--- Razerbot/modules/google.py
+++ Razerbot/modules/google.py
@@ -88,7 +88,7 @@ useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) 
 opener.addheaders = [("User-agent", useragent)]
 
 
-@register(pattern="^[!/.]reverse(?: |$)(\d*)")
+@register(pattern="^[!/.](reverse|grs)(?: |$)(\d*)")
 async def okgoogle(img):
     """ For .reverse command, Google search images and stickers. """
     if os.path.isfile("okgoogle.png"):
