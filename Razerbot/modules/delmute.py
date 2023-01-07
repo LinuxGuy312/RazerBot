e8875ccfbee39c748a6f9ef581cf298eb0202d11
LinuxGuy312 <githubwh@gmail.com>
2023-01-07 12:23:02 +0530

    ye bhi thik hai

--- Razerbot/modules/delmute.py
+++ Razerbot/modules/delmute.py
@@ -1,13 +1,13 @@
 import asyncio
-from Razerbot.modules.sql.mute_sql import *
 from Razerbot.events import register
-from Razerbot import telethn as tbot, EVENT_LOGS, LOGGER, OWNER_ID
+from Razerbot import telethn as tbot, pbot, EVENT_LOGS, LOGGER, OWNER_ID
 from telethon.tl.types import MessageEntityMentionName
 from telethon import events
 
 EVENT_LOGGER = True
+DMUTE_LIST = []
 
-@tbot.on(events.NewMessage(incoming=True))
+@pbot.on(events.NewMessage(incoming=True))
 async def watcher(event):
     if is_muted(event.sender_id, event.chat_id):
         try:
@@ -88,7 +88,7 @@ async def get_user_from_event(
 async def delmute(event):
     userid = event.sender.id
     perm = await tbot.get_permissions(event.chat_id, userid)
-    if not perm.is_admin or userid != OWNER_ID:
+    if not (perm.is_admin or userid == OWNER_ID):
         return await event.reply("This command is only for admins.")
     if event.is_private:
         return await event.reply("How can you be so noob? :/")
@@ -103,9 +103,11 @@ async def delmute(event):
         return
     if user.id == myid:
         return await event.reply("Sorry, I can't mute myself.")
+    if user.id == userid:
+        return await event.reply("Trying to mute yourself? Not gonna happen")
     if user.id == OWNER_ID:
         return await event.reply("Nice Try Muting my owner right there XD")
-    if is_muted(user.id, event.chat_id):
+    if user.id in DMUTE_LIST:
         return await event.reply("`This user is already muted in this chat ~~lmfao sed rip~~`")
     result = await tbot.get_permissions(event.chat_id, user.id)
     try:
@@ -121,7 +123,7 @@ async def delmute(event):
             return await event.reply("`I can't mute a person if I dont have delete messages permission. ಥ﹏ಥ`")
     elif "creator" not in vars(chat):
         return await event.reply("`I can't mute a person without having admin rights.` ಥ﹏ಥ  ")
-    mute(user.id, event.chat_id)
+    DMUTE_LIST.append[int(user.id)]
     if reason:
         await event.reply(
             f"[{user.first_name}]({unid}) is muted in {event.chat.title}\n"
@@ -141,7 +143,7 @@ async def delmute(event):
 async def undelmute(event):
     userid = event.sender.id
     perm = await tbot.get_permissions(event.chat_id, userid) 
-    if not perm.is_admin or userid != OWNER_ID:
+    if not (perm.is_admin or userid == OWNER_ID):
         return await event.reply("This command is only for admins.")
     if event.is_private:
         return await event.reply("How can you be so noob? :/")
@@ -150,8 +152,8 @@ async def undelmute(event):
         return
     try:
         unid = f"@{user.username}" if user.username is not None else f"tg://user?id={user.id}"
-        if is_muted(user.id, event.chat_id):
-            unmute(user.id, event.chat_id)
+        if user.id in DMUTE_LIST:
+            DMUTE.remove(int(user.id))
             await event.reply(f"[{user.first_name}]({unid}) is unmuted in {event.chat.title}")
         else:
             return await event.reply("`This user can already speak freely in this chat`")
