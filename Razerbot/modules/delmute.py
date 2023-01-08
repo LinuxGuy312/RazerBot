fd5b481b4c1a523ee0ecbc7eed1c4d6190d7d44e
LinuxGuy312 <githubwh@gmail.com>
2023-01-08 17:08:34 +0530

    bc

--- Razerbot/modules/delmute.py
+++ Razerbot/modules/delmute.py
@@ -8,7 +8,7 @@ EVENT_LOGGER = True
 
 @pbot.on_message(group=1)
 async def watcher(_, m):
-    if is_muted(m.from_user.id, m.chat_id):
+    if is_muted(m.from_user.id, m.chat.id):
         await m.delete()
 
 
