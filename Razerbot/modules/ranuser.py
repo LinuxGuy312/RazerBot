fb322468913751995e0504def72aa2124a7a9f4c
LinuxGuy312 <githubwh@gmail.com>
2023-01-18 18:18:21 +0530

    try it out

--- Razerbot/modules/ranuser.py
+++ Razerbot/modules/ranuser.py
@@ -2,21 +2,20 @@ import requests
 import random
 import os
 import json
+from faker import Faker
 from datetime import date
 from Razerbot import telethn as tbot
 from Razerbot.events import register
 
-@register(pattern="^[!/.]ranuser")
+@register(pattern="^[!/]ranuser")
 async def gen(event):
-	bhurr = await event.edit("ɢᴇɴᴇʀᴀᴛɪɴɢ ɪɴꜰᴏ...")
+	fake = Faker()
+	bhurr = await event.reply("ɢᴇɴᴇʀᴀᴛɪɴɢ ɪɴꜰᴏ...")
 	yr = date.today().year
 	cyr = int(yr)
 	inf = "https://randomuser.me/api"
-	ccinf = "https://random-data-api.com/api/v2/credit_cards"
 	rinf = requests.get(inf)
-	rccinf = requests.get(ccinf)
 	injs = rinf.json()
-	ccinjs = rccinf.json()
 	tt, fn, ln = injs['results'][0]['name']['title'], injs['results'][0]['name']['first'], injs['results'][0]['name']['last']
 	gender = injs['results'][0]['gender']
 	name = f"{tt} {fn} {ln}"
@@ -28,9 +27,9 @@ async def gen(event):
 	pscd = injs['results'][0]['location']['postcode']
 	email = injs['results'][0]['email'].replace("example", "email")
 	cell = injs['results'][0]['cell']
-	ccnum = ccinjs['credit_card_number']
-	ccexp = ccinjs['credit_card_expiry_date']
-	cvv = random.choice(range(100,999))
+	ccnum = fake.credit_card_number(card_type='visa16')
+	ccexp = fake.credit_card_expire(card_type='visa16')
+	cvv = fake.credit_card_security_code(card_type='visa16')
 	poli = f"https://fakeface.rest/face/json?gender={gender}&minimum_age=18&maximum_age=40"
 	rpoli = requests.get(poli)
 	poinjs = rpoli.json()
@@ -42,4 +41,4 @@ async def gen(event):
 	dob = f"{dyr}{dfm}"
 	capt = f"**ɴᴀᴍᴇ:** {name}\n**ᴅᴏʙ:** {dob}\n**sᴛʀᴇᴇᴛ:** {street}\n**ᴄɪᴛʏ:** {city}\n**sᴛᴀᴛᴇ:** {state}\n**ᴄᴏᴜɴᴛʀʏ:** {country}\n**ᴘᴏsᴛᴀʟ ᴄᴏᴅᴇ:** {pscd}\n**ᴇᴍᴀɪʟ:** {email}\n**ᴘʜᴏɴᴇ:** {cell}\n\n**ᴄᴄ ɪɴꜰᴏ:**\n    **ᴄᴄ ɴᴜᴍʙᴇʀ:** {ccnum}\n    **ᴇxᴘɪʀʏ:** {ccexp}\n    **ᴄᴠᴠ:** {cvv}"
 	await bhurr.delete()
-	await tbot.send_file(event.chat_id, poto, caption=capt, reply_to=event.reply_to_msg_id, force_document=False)
+	await tbot.send_file(event.chat_id, poto, caption=capt, reply_to=event.id, force_document=False)
