cea56b9147a459eb8918c22ac390906edf3b1e02
LinuxGuy312 <githubwh@gmail.com>
2023-02-22 19:24:39 +0530

    Nice

--- Razerbot/modules/eval.py
+++ Razerbot/modules/eval.py
@@ -1,50 +1,105 @@
-import os
 import re
-import subprocess
+import io
+import os
 import sys
 import traceback
-from inspect import getfullargspec
-from io import StringIO
-from time import time
-from Razerbot.utils import format as _format
+import asyncio 
+import time
+from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
 from Razerbot import DRAGONS, pbot as app
 
 
-async def aexec(code, client, message):
-    p = lambda _x: print(_format.yaml_format(_x))
-    exec(
-        "async def __aexec(client, message): "
-        + "".join(f"\n {a}" for a in code.split("\n"))
+def utc_to_local(utc_datetime):
+    now_timestamp = time.time()
+    offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(
+        now_timestamp
     )
-    return await locals()["__aexec"](client, message)
+    return utc_datetime + offset
+
+def yaml_format(obj, indent=0, max_str_len=256, max_byte_len=64):
+    result = []
+
+    if isinstance(obj, dict):
+        if not obj:
+            return "dict:"
+        items = obj.items()
+        has_items = len(items) > 1
+        has_multiple_items = len(items) > 2
+        result.append(obj.get("_", "dict") + (":" if has_items else ""))
+        if has_multiple_items:
+            result.append("\n")
+            indent += 2
+        for k, v in items:
+            if k == "_" or v is None:
+                continue
+            formatted = yaml_format(v, indent)
+            if not formatted.strip():
+                continue
+            result.append(" " * (indent if has_multiple_items else 1))
+            result.append(f"{k}:")
+            if not formatted[0].isspace():
+                result.append(" ")
+            result.append(f"{formatted}")
+            result.append("\n")
+        if has_items:
+            result.pop()
+        if has_multiple_items:
+            indent -= 2
+    elif isinstance(obj, str):
+        result = repr(obj[:max_str_len])
+        if len(obj) > max_str_len:
+            result += "…"
+        return result
+    elif isinstance(obj, bytes):
+        if all(0x20 <= c < 0x7F for c in obj):
+            return repr(obj)
+        return "<…>" if len(obj) > max_byte_len else " ".join(f"{b:02X}" for b in obj)
+    elif isinstance(obj, datetime):
+        return utc_to_local(obj).strftime("%Y-%m-%d %H:%M:%S")
+    elif hasattr(obj, "__iter__"):
+        result.append("\n")
+        indent += 2
+        for x in obj:
+            result.append(f"{' ' * indent}- {yaml_format(x, indent + 2)}")
+            result.append("\n")
+        result.pop()
+        indent -= 2
+    else:
+        return repr(obj)
+    return "".join(result)
+  
 
 
-async def edit_or_reply(msg: Message, **kwargs):
-    func = msg.edit_text if msg.from_user.is_self else msg.reply
-    spec = getfullargspec(func.__wrapped__).args
-    await func(**{k: v for k, v in kwargs.items() if k in spec})
-
+async def aexec_(code, smessatatus, client):
+    message = event = smessatatus
+    p = lambda _x: print(yaml_format(_x))
+    exec("async def __aexec(message, event, client, p): "
+            + "".join(f"\n {l}" for l in code.split("\n")))
+  
+    return await locals()["__aexec"](
+        message, event, client, p
+    )
+  
 
 @app.on_edited_message(filters.command("eval", prefixes=["/", "!"]) & filters.user(DRAGONS) & ~filters.forwarded)
 @app.on_message(filters.command("eval", prefixes=["/", "!"]) & filters.user(DRAGONS) & ~filters.forwarded)
-async def executor(client, message):
-    if len(message.command) < 2:
-        return await edit_or_reply(message, text="ᴡʜᴀᴛ sʜᴏᴜʟᴅ ɪ ʀᴜɴ?")
-    try:
-        cmd = "".join(message.text.split(maxsplit=1)[1:])
-    except IndexError:
-        return await message.delete()
-    t1 = time()
+async def eval(client, message):
+    if event.sender.id not in DEV_USERS:
+        return await event.reply("ᴛʜɪs ɪs ᴀ ᴅᴇᴠᴇʟᴏᴘᴇʀ ʀᴇsᴛʀɪᴄᴛᴇᴅ ᴄᴏᴍᴍᴀɴᴅ.\nʏᴏᴜ ᴅᴏ ɴᴏᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴs ᴛᴏ ʀᴜɴ ᴛʜɪs.")
+    cmd = "".join(message.text.split(maxsplit=1)[1:])
+    print(cmd)
+    if not cmd:
+        return await message.reply_text("`What should i run?..`")
+    eval_ = await message.reply_text("`Running ...`")
     old_stderr = sys.stderr
     old_stdout = sys.stdout
-    redirected_output = sys.stdout = StringIO()
-    redirected_error = sys.stderr = StringIO()
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
     stdout, stderr, exc = None, None, None
     try:
-        await aexec(cmd, client, message)
+        await aexec_(cmd, message, client)
     except Exception:
         exc = traceback.format_exc()
     stdout = redirected_output.getvalue()
@@ -59,10 +114,8 @@ async def executor(client, message):
     elif stdout:
         evaluation = stdout
     else:
-        evaluation = "sᴜᴄᴄᴇss"
-    final_output = (
-        f"⥤ ᴇᴠᴀʟ : \n```{cmd}``` \n\n⥤ ʀᴇsᴜʟᴛ : \n```{evaluation}``` \n"
-    )
+        evaluation = "Success"
+    final_output = f"**•  Eval : **\n`{cmd}` \n\n**•  Output :**\n`{evaluation}`\n"
     if len(final_output) > 4096:
         filename = "output.txt"
         with open(filename, "w+", encoding="utf8") as out_file:
@@ -93,7 +146,7 @@ async def executor(client, message):
                 [
                     InlineKeyboardButton(
                         text="⏳",
-                        callback_data=f"runtime {round(t2-t1, 3)} Seconds",
+                        callback_data=f"Runtime {round(t2-t1, 3)} Seconds",
                     ),
                     InlineKeyboardButton(
                         text="🗑",
@@ -102,85 +155,9 @@ async def executor(client, message):
                 ]
             ]
         )
-        await edit_or_reply(message, text=final_output, reply_markup=keyboard)
-
-
+        await message.reply(text=final_output, reply_markup=keyboard)
+    
 @app.on_callback_query(filters.regex(r"runtime"))
 async def runtime_func_cq(_, cq):
     runtime = cq.data.split(None, 1)[1]
     await cq.answer(runtime, show_alert=True)
-
-
-@app.on_edited_message(
-    filters.command("sh")
-    & filters.user(DRAGONS)
-    & ~filters.forwarded
-    & ~filters.via_bot
-)
-@app.on_message(
-    filters.command("sh")
-    & filters.user(DRAGONS)
-    & ~filters.forwarded
-    & ~filters.via_bot
-)
-async def shellrunner(client, message):
-    if len(message.command) < 2:
-        return await edit_or_reply(message, text="**ᴇxᴀᴍᴩʟᴇ :**\n/sh git pull")
-    text = message.text.split(None, 1)[1]
-    if "config.py" in text:
-        return await message.reply_text(f"`{BOT_USERNAME}:~# {cmd}`\n`bash: Can't access config.py`")
-    if "\n" in text:
-        code = text.split("\n")
-        output = ""
-        for x in code:
-            shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", x)
-            try:
-                process = subprocess.Popen(
-                    shell,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                )
-            except Exception as err:
-                print(err)
-                await edit_or_reply(message, text=f"**ᴇʀʀᴏʀ:**\n```{err}```")
-            output += f"**{code}**\n"
-            output += process.stdout.read()[:-1].decode("utf-8")
-            output += "\n"
-    else:
-        shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", text)
-        for a in range(len(shell)):
-            shell[a] = shell[a].replace('"', "")
-        try:
-            process = subprocess.Popen(
-                shell,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-        except Exception as err:
-            print(err)
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            errors = traceback.format_exception(
-                etype=exc_type,
-                value=exc_obj,
-                tb=exc_tb,
-            )
-            return await edit_or_reply(
-                message, text=f"**ᴇʀʀᴏʀ:**\n```{''.join(errors)}```"
-            )
-        output = process.stdout.read()[:-1].decode("utf-8")
-    if str(output) == "\n":
-        output = None
-    if output:
-        if len(output) > 4096:
-            with open("output.txt", "w+") as file:
-                file.write(output)
-            await client.send_document(
-                message.chat.id,
-                "output.txt",
-                reply_to_message_id=message.message_id,
-                caption="`Output`",
-            )
-            return os.remove("output.txt")
-        await edit_or_reply(message, text=f"**ᴏᴜᴛᴘᴜᴛ:**\n```{output}```")
-    else:
-        await edit_or_reply(message, text="**ᴏᴜᴛᴘᴜᴛ: **\n`ɴᴏ ᴏᴜᴛᴘᴜᴛ`")
