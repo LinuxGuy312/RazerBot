2abacc7014e422b2fcd0e4cfe0ebbea7f288913a
LinuxGuy312 <githubwh@gmail.com>
2023-02-22 19:24:49 +0530

    Nice

---
+++ Razerbot/modules/sysd.py
@@ -0,0 +1,13 @@
+import subprocess
+from Razerbot import pbot
+
+@pbot.on_message(filters.regex('^[/!](sysd|neofetch)'))
+@pbot.on_edited_message(filters.regex('^[/!](sysd|neofetch)'))
+async def neofetch(bot, m):
+    cmd = 'neofetch --off --color_blocks off --bold off --cpu_temp C --cpu_speed on --cpu_cores physical --kernel_shorthand off --stdout'
+    process = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+    )
+    stdout, _ = process.communicate()
+    out = stdout.decode()
+    m.reply_text(out)
